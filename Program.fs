578395fcc8dd9ba21050701be156e2a08b1f5252
Henrique Kleinberger <henriquedfkl@gmail.com>
2022-07-01 13:08:14 +0300

    method fitBoltsAndNuts with bag parameter created :hammer:

--- Program.fs
+++ Program.fs
@@ -1,4 +1,4 @@
-﻿
+﻿module Exercice
 type Type =
 | Nut
 | Bolt
@@ -22,41 +22,43 @@ let bag = [
     { Type = Bolt; Size = 3 }
     { Type = Nut; Size = 3 }
 ]
+let fitBoltsAndNuts = fun (items: Item list) ->
+    let (bolts, nuts) = List.partition (fun (item : Item) -> item.Type = Type.Bolt) bag
 
-let (bolts, nuts) = List.partition (fun (item : Item) -> item.Type = Type.Bolt) bag
-
-let rec fitBoltsAndNuts = fun (bolts: Item list) (nuts: Item list) ->
-    match bolts with
-    | [] -> []
-    | boltPivot::othersBolts ->
-        let partitionSmallerEqualBigger (list: Item list) (pivot: Item) =
-          let rec loop (l: Item list) (i: Item)= 
-            match l with 
-            | [] -> [], [], []
-            | firstItem::othersItems ->
-                let l1, l2, l3 = loop othersItems pivot
-                if firstItem.Size < i.Size then firstItem::l1, l2, l3
-                elif firstItem.Size > i.Size then l1, l2, firstItem::l3
-                else l1, firstItem::l2 ,l3
-          loop list pivot
+    let rec fitBoltsAndNuts = fun (bolts: Item list) (nuts: Item list) ->
+        match bolts with
+        | [] -> []
+        | boltPivot::othersBolts ->
+            let partitionSmallerEqualBigger (list: Item list) (pivot: Item) =
+              let rec loop (l: Item list) (i: Item)= 
+                match l with 
+                | [] -> [], [], []
+                | firstItem::othersItems ->
+                    let l1, l2, l3 = loop othersItems pivot
+                    if firstItem.Size < i.Size then firstItem::l1, l2, l3
+                    elif firstItem.Size > i.Size then l1, l2, firstItem::l3
+                    else l1, firstItem::l2 ,l3
+              loop list pivot
         
-        // arrange the nuts
-        let (smallerNuts, equalsNuts, biggerNuts) = partitionSmallerEqualBigger nuts boltPivot
+            // arrange the nuts
+            let (smallerNuts, equalsNuts, biggerNuts) = partitionSmallerEqualBigger nuts boltPivot
 
-        // take the equal nut and arrange the bolts
-        let nutPivot = equalsNuts.Head;
-        let (smallerBolts, equalsBolts, biggerBolts) = partitionSmallerEqualBigger othersBolts nutPivot
+            // take the equal nut and arrange the bolts
+            let nutPivot = equalsNuts.Head;
+            let (smallerBolts, equalsBolts, biggerBolts) = partitionSmallerEqualBigger othersBolts nutPivot
 
-        // recursion
-        let smaller = fitBoltsAndNuts smallerNuts smallerBolts
-        let bigger = fitBoltsAndNuts biggerBolts biggerNuts
+            // recursion
+            let smaller = fitBoltsAndNuts smallerNuts smallerBolts
+            let bigger = fitBoltsAndNuts biggerBolts biggerNuts
 
-        let equals = List.zip (boltPivot::equalsBolts) equalsNuts
+            let equals = List.zip (boltPivot::equalsBolts) equalsNuts
 
-        smaller@equals@bigger
+            smaller@equals@bigger
+        
+    fitBoltsAndNuts bolts nuts
 
 
-let result = fitBoltsAndNuts bolts nuts
+let result = fitBoltsAndNuts bag
 
 // print the pairs that fits
 List.map(fun ((bolt: Item), (nut: Item)) -> eprintfn $"{bolt.Type}: {bolt.Size} fits into {nut.Type}: {nut.Size}") result
